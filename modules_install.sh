e831828087e0831722fcba5fb56e0b4215d4e3be
nicocomomolo <38205536+nicocomomolo@users.noreply.github.com>
2018-04-11 10:29:13 +0200

    Update modules_install.sh

--- modules_install.sh
+++ modules_install.sh
@@ -29,7 +29,7 @@ sudo git clone --branch $OE_VERSION https://github.com/OCA/bank-statement-import
 
 echo -e "\n---- Create some links ----"
 sudo ln -s $OE_HOME/account-financial-reporting/account_export_csv $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-reporting/account_move_line_report_xls $OE_HOME/custom/addons/
+sudo ln -s $OE_HOME/account-financial-reporting/account_move_line_report_xlsx $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-financial-tools/account_chart_update $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-financial-tools/account_invoice_constraint_chronology $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-financial-tools/account_invoice_currency $OE_HOME/custom/addons/
@@ -39,19 +39,16 @@ sudo ln -s $OE_HOME/account-payment/account_due_list $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/account-payment/account_due_list_payment_mode $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_banking_mandate/ $OE_HOME/custom/addons/ 
 sudo ln -s $OE_HOME/bank-payment/account_banking_pain_base/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_banking_payment_export/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_banking_payment_transfer/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_banking_sepa_direct_debit/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_direct_debit/ $OE_HOME/custom/addons/
+sudo ln -s $OE_HOME/bank-payment/account_banking_sepa_credit_transfer/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/bank-payment/account_payment_partner $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-statement-import/account_bank_statement_import/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/attachment_preview/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/attachments_to_filesystem/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/document_page/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/knowledge/document_url/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/account_balance_reporting_xls/ $OE_HOME/custom/addons/
+sudo ln -s $OE_HOME/l10n-spain/account_balance_reporting_xlsx/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/account_balance_reporting/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/account_refund_original $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_account_asset/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_account_balance_report/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_account_bank_statement_import_n43/ $OE_HOME/custom/addons/
@@ -65,7 +62,6 @@ sudo ln -s $OE_HOME/l10n-spain/l10n_es_aeat_mod340/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_aeat_mod347/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_aeat_mod349/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_aeat/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_fiscal_year_closing/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_partner_mercantil/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_partner/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/l10n-spain/l10n_es_pos/ $OE_HOME/custom/addons/
@@ -75,9 +71,7 @@ sudo ln -s $OE_HOME/partner-contact/base_location $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/partner-contact/base_location_geonames_import/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/partner-contact/base_partner_sequence $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/pos/pos_pricelist/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/reporting-engine/report_custom_filename/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/reporting-engine/report_xls/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/server-tools/disable_openerp_online/ $OE_HOME/custom/addons/
+sudo ln -s $OE_HOME/reporting-engine/report_xlsx/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/server-tools/mass_editing/ $OE_HOME/custom/addons/
 sudo ln -s $OE_HOME/web/web_export_view/ $OE_HOME/custom/addons/
 
@@ -88,5 +82,4 @@ sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
 
 
 echo "Done! Some modules installed"
-
 
