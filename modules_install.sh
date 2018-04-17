c35e8765eb0e3254b5d3420409d49348fa767e38
nicocomomolo <38205536+nicocomomolo@users.noreply.github.com>
2018-04-17 12:06:57 +0200

    Update modules_install.sh

--- modules_install.sh
+++ modules_install.sh
@@ -1,86 +1,77 @@
 #!/bin/bash
-################################################################################
-# Script for Installation: some odoo modules
-################################################################################
- 
-##fixed parameters
-#openerp
-OE_USER="odoo"
-OE_HOME="/opt/$OE_USER"
 
-#Enter version for checkout "8.0" for version 8.0, "7.0 (version 7), saas-4, saas-5 (opendays version) and "master" for trunk
-OE_VERSION="10.0"
-
-echo -e "\n==== Installing spanish localization ===="
-sudo git clone --branch $OE_VERSION https://github.com/OCA/l10n-spain.git $OE_HOME/l10n-spain
-
-echo -e "\n==== Installing some modules ===="
-sudo git clone --branch $OE_VERSION https://github.com/OCA/account-financial-tools.git $OE_HOME/account-financial-tools
-sudo git clone --branch $OE_VERSION https://github.com/OCA/account-financial-reporting.git $OE_HOME/account-financial-reporting
-sudo git clone --branch $OE_VERSION https://github.com/OCA/account-invoicing.git $OE_HOME/account-invoicing
-sudo git clone --branch $OE_VERSION https://github.com/OCA/account-payment.git $OE_HOME/account-payment
-sudo git clone --branch $OE_VERSION https://github.com/OCA/knowledge.git $OE_HOME/knowledge
-sudo git clone --branch $OE_VERSION https://github.com/OCA/reporting-engine.git $OE_HOME/reporting-engine
-sudo git clone --branch $OE_VERSION https://github.com/OCA/partner-contact.git $OE_HOME/partner-contact
-sudo git clone --branch $OE_VERSION https://github.com/OCA/server-tools.git $OE_HOME/server-tools
-sudo git clone --branch $OE_VERSION https://github.com/OCA/web.git $OE_HOME/web
-sudo git clone --branch $OE_VERSION https://github.com/OCA/bank-payment.git $OE_HOME/bank-payment
-sudo git clone --branch $OE_VERSION https://github.com/OCA/pos.git $OE_HOME/pos
-sudo git clone --branch $OE_VERSION https://github.com/OCA/bank-statement-import.git $OE_HOME/bank-statement-import
-
-echo -e "\n---- Create some links ----"
-sudo ln -s $OE_HOME/account-financial-reporting/account_export_csv $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-reporting/account_move_line_report_xls $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-reporting/account_tax_balance $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-tools/account_chart_update $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-tools/account_tax_balance $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-tools/account_invoice_constraint_chronology $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-tools/account_invoice_currency $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-invoicing/account_invoice_refund_link $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-financial-tools/account_renumber $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-payment/account_due_list $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/account-payment/account_due_list_payment_mode $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_banking_mandate/ $OE_HOME/custom/addons/ 
-sudo ln -s $OE_HOME/bank-payment/account_banking_pain_base/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_banking_sepa_direct_debit/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_banking_sepa_credit_transfer/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_payment_mode/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_payment_order/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_payment_partner $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-payment/account_banking_sepa_direct_debit $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/knowledge/attachment_preview/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/knowledge/attachments_to_filesystem/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/knowledge/document_page/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/knowledge/document_url/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/knowledge/knowledge/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/account_balance_reporting_xlsx/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/account_balance_reporting/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_account_asset/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_account_balance_report/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_account_bank_statement_import_n43/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_aeat/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_partner_mercantil/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_partner/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_pos/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es_toponyms/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/l10n-spain/l10n_es/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/partner-contact/base_location $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/partner-contact/base_location_geonames_import/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/partner-contact/base_partner_sequence $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/reporting-engine/report_xlsx/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/server-tools/mass_editing/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/server-tools/date_range/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/web/web_export_view/ $OE_HOME/custom/addons/
-sudo ln -s $OE_HOME/bank-statement-import/account_bank_statement_import/ $OE_HOME/custom/addons/
+# 1.-Descarga
+sudo git clone https://github.com/OCA/l10n-spain.git -b 10.0 /opt/odoo/l10n-spain
+sudo git clone https://github.com/OCA/account-financial-tools.git -b 10.0 /opt/odoo/account-financial-tools
+sudo git clone https://github.com/OCA/account-financial-reporting.git -b 10.0 /opt/odoo/account-financial-reporting
+sudo git clone https://github.com/OCA/account-payment.git -b 10.0 /opt/odoo/account-payment
+sudo git clone https://github.com/OCA/knowledge.git -b 10.0 /opt/odoo/knowledge
+sudo git clone https://github.com/OCA/reporting-engine.git -b 10.0 /opt/odoo/reporting-engine
+sudo git clone https://github.com/OCA/partner-contact.git -b 10.0 /opt/odoo/partner-contact
+sudo git clone https://github.com/OCA/server-tools.git -b 10.0 /opt/odoo/server-tools
+sudo git clone https://github.com/OCA/web.git -b 10.0 /opt/odoo/web
+sudo git clone https://github.com/OCA/bank-payment.git -b 10.0 /opt/odoo/bank-payment
+sudo git clone https://github.com/OCA/bank-statement-import.git -b 10.0 /opt/odoo/bank-statement-import
+# Repositorio instalador
+sudo git clone https://github.com/cursoerp2018/install.git -b 10.0 /opt/odoo/install
 
 
-
-
-
-
-echo -e "\n---- Setting permissions on home folder ----"
-sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
-
-
-echo "Done! Some modules installed"
-
+# 2.-Acceso directo
+sudo ln -s /opt/odoo/account-financial-reporting/account_export_csv /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-reporting/account_move_line_report_xls /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-reporting/account_tax_balance /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_chart_update /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_invoice_constraint_chronology /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_invoice_currency /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_journal_always_check_date /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-financial-tools/account_renumber /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-payment/account_due_list /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/account-payment/account_due_list_payment_mode /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/bank-payment/account_banking_mandate /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/bank-payment/account_payment_order /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/bank-payment/account_payment_mode /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/bank-payment/account_banking_pain_base /opt/odoo/custom/addons
+#sudo ln -s /opt/odoo/bank-payment/account_banking_payment_export /opt/odoo/custom/addons
+#sudo ln -s /opt/odoo/bank-payment/account_banking_payment_transfer /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/bank-payment/account_banking_sepa_direct_debit /opt/odoo/custom/addons
+#sudo ln -s /opt/odoo/bank-payment/account_direct_debit /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/bank-payment/account_payment_partner /opt/odoo/custom/addons
+#sudo ln -s /opt/odoo/bank-statement-import/account_bank_statement_import /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/knowledge/attachment_preview /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/knowledge/attachments_to_filesystem /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/knowledge/document_page /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/knowledge/document_url /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/knowledge/knowledge /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/account_balance_reporting_xlsx /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/account_balance_reporting /opt/odoo/custom/addons
+#sudo ln -s /opt/odoo/l10n-spain/account_refund_original /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_asset /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_balance_report /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_bank_statement_import_n43 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_invoice_sequence /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod111 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod115 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod130 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod303 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod340_type0 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod340 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod347 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat_mod349 /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_aeat /opt/odoo/custom/addons
+#sudo ln -s /opt/odoo/l10n-spain/l10n_es_account_fiscal_year_closing /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_partner_mercantil /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_partner /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_pos /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es_toponyms /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/l10n-spain/l10n_es /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/partner-contact/base_location /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/partner-contact/base_location_geonames_import /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/partner-contact/base_partner_sequence /opt/odoo/custom/addons
+#sudo ln -s /opt/odoo/reporting-engine/report_custom_filename /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/reporting-engine/report_xlsx /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/server-tools/disable_odoo_online /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/server-tools/mass_editing /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/server-tools/date_range /opt/odoo/custom/addons
+sudo ln -s /opt/odoo/web/web_export_view /opt/odoo/custom/addons
+# Módulo instalador
+sudo ln -s /opt/odoo/install/oca_install /opt/odoo/custom/addons
